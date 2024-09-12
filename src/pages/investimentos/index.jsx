f8d5265faae4395c5eeb1cac3a1c331b8b1b07fd
Nícolas Ferreira <143196040+NickSilva71@users.noreply.github.com>
2024-09-12 14:19:55 -0300

    Delete src directory

--- src/pages/investimentos/index.jsx
+++
@@ -1,7 +0,0 @@
-export default function Investimentos() {
-  return (
-    <div className="flex flex-col font-medium text-2xl text-center p-6 max-w-fit m-auto border-r-2 border-b-2 border-teal-600 bg-teal-100 rounded-xl shadow-lg">
-      <h1>Pagina de Investimentos</h1>
-    </div>
-  );
-}
--- src/pages/notificacoes/index.jsx
+++
@@ -1,7 +0,0 @@
-export default function Notificacoes() {
-  return (
-    <div className="flex flex-col font-medium text-2xl text-center p-6 max-w-fit m-auto border-r-2 border-b-2 border-teal-600 bg-teal-100 rounded-xl shadow-lg">
-      <h1>Pagina de Notificações</h1>
-    </div>
-  );
-}
