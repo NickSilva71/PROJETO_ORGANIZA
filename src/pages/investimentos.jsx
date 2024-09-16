ec1dc64f960ae37dbd29a16afe7025fce8de5151
Renato Conrado <renatoconrado38@gmail.com>
2024-09-15 21:44:31 -0300

    Siderbar em tailwindCSS substituida por Sidebar usando Chakra UI

--- src/pages/investimentos.jsx
+++ src/pages/investimentos.jsx
@@ -6,12 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Investimentos() {
   return (
-    <div>
-      <Sidebar />
-      <div className="flex flex-col font-medium text-2xl text-center p-6 max-w-fit m-auto border-r-2 border-b-2 border-teal-600 bg-teal-100 rounded-xl shadow-lg">
-        <h1>Pagina de Investimentos</h1>
-      </div>
-    </div>
+    <Sidebar />
   );
 }
 
